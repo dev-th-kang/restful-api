0b51d14cc0ecf6ef63cbf77605286fe2f64fa02e
dev-th-kang <kang.1107.th@gmail.com>
2022-09-23 05:17:06 +0900

    db conn부 구현 및 DOTENV로 보안성 올리기

--- api/join/join.js
+++ api/join/join.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const routes = express.Router();
 
+const express = require('express');
+const db = require('../../config/db');
+const routes = express.Router();
 routes.post("/", (req,res)=>{
     //let userName = req.body.userName;
-    console.log(req.body)
+    let userInfo = req.body;
+    console.log(userInfo.id);
+    db.query(`select * from test WHERE userid = "${userInfo.id}"`, (err, results)=>{
+        if(err)throw err;
+        if(results.length == 0 ) res.send({state:"사용 가능한 아이디입니다."});
+        else res.send({state:"이미 사용중인 아이디입니다."});
+    });
 })
 
 module.exports = routes;
--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require("body-parser")
 const routes = require('./api');
 const app = express();
-
+const db = require('./config/db')
 
 //아래의 내용을 안넣으면 body 값을 요청할때
 //undefined라고 나옴. 
+db.connect((err)=>{  if(!err) console.log("Good!");});
 app.use(bodyParser.json()) 
 app.use(bodyParser.urlencoded({extended: false})) 
 
