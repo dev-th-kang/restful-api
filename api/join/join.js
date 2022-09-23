4cb64bb000d458bb4dcf209620f58d0008af8d42
dev-th-kang <kang.1107.th@gmail.com>
2022-09-23 17:48:02 +0900

    feat:passport없이 join 구현

--- api/join/join.js
+++ api/join/join.js
@@ -2,15 +2,32 @@
 const express = require('express');
 const db = require('../../config/db');
 const routes = express.Router();
+
+//TODO: 아이디 유효성 검사(중복확인)
+routes.post("/valid-id", (req,res)=>{
+    let userid = req.body.userid;
+    db.query(`select * from test WHERE userid = "${userid}"`, (err, results)=>{
+        if(results.length == 0 ){
+            res.send({isvalid:true});
+        }else{
+            res.send({isvalid:false});
+        }
+    })
+})
+
+//FIXME: passport 를 사용하기
+//TODO: 아이디 가입
 routes.post("/", (req,res)=>{
     //let userName = req.body.userName;
     let userInfo = req.body;
-    console.log(userInfo.id);
-    db.query(`select * from test WHERE userid = "${userInfo.id}"`, (err, results)=>{
-        if(err)throw err;
-        if(results.length == 0 ) res.send({state:"사용 가능한 아이디입니다."});
-        else res.send({state:"이미 사용중인 아이디입니다."});
-    });
+    if(userInfo.isvalid){
+        db.query(`insert into test(username,userid,userpw,email) values ("${userInfo.username}","${userInfo.userid}","${userInfo.userpw}","${userInfo.email}")`,(err, results)=>{
+            if(err)throw err;
+            res.send({state:"success"});
+        });
+    }else{
+        res.send({state:"fail"});
+    }
 })
 
 module.exports = routes;
