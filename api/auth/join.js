364154a8aa326d491ddc4c51798944347fe11887
dev-th-kang <kang.1107.th@gmail.com>
2022-09-28 03:03:47 +0900

    feature/passport-local: strategy분리/ sesrialize 함수 분리

--- api/auth/join.js
+++ api/auth/join.js
@@ -1,35 +1,53 @@
-
 const express = require('express');
 const db = require('../../config/db');
 const routes = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+require('../../passport/serialize');
+require('../../passport/passport')(passport);
 
 //TODO: 아이디 유효성 검사(중복확인)
-routes.post("/valid-id", (req,res)=>{
-    let userid = req.body.userid;
-    db.query(`select * from test WHERE userid = "${userid}"`, (err, results)=>{
-        if(results.length == 0 ){
-            res.status(400).send({isvalid:true});
-        }else{
-            res.status(200).send({isvalid:false});
-        }
-    })
-})
+// routes.post("/valid-id", (req,res)=>{
+//     let userid = req.body.userid;
+//     db.query(`select * from test WHERE userid = "${userid}"`, (err, results)=>{
+//         if(results.length == 0 ){
+//             res.status(400).send({isvalid:true});
+//         }else{
+//             res.status(200).send({isvalid:false});
+//         }
+//     })
+// })
 
 //FIXME: passport 를 사용하기
 //TODO: 아이디 가입
-routes.post("/", (req,res)=>{
-    //let userName = req.body.userName;
-    let userInfo = req.body;
-    if(userInfo.isvalid){
-        db.connect();
-        db.query(`insert into test(username,userid,userpw,email) values ("${userInfo.username}","${userInfo.userid}","${userInfo.userpw}","${userInfo.email}")`,(err, results)=>{
-            if(err)throw err;
-            res.status(400).send({state:"success"});
-        });
-        db.end();
-    }else{
-        res.status(200).send({state:"fail"});
-    }
+// routes.post("/", (req,res)=>{
+//     //let userName = req.body.userName;
+//     let userInfo = req.body;
+//     if(userInfo.isvalid){
+//         db.query(`insert into test(username,userid,userpw,email) values ("${userInfo.username}","${userInfo.userid}","${userInfo.userpw}","${userInfo.email}")`,(err, results)=>{
+//             if(err)throw err;
+//             res.status(400).send({state:"success"});
+//         });
+//     }else{
+//         res.status(200).send({state:"fail"});
+//     }
+// });
+
+routes.post('/', (req,res,next)=>{
+    passport.authenticate('local-join',(err,userinfo,msg)=>{
+        if(!userinfo){
+            res.send(msg);
+        }else{
+            console.log(`${userinfo.username} 님이 가입되었습니다.`);
+            res.send({"msg":"join succeed"});
+            req.logIn(userinfo,(err)=>{
+                
+            })
+        }
+    })(req,res,next);
 })
+
+
+
 
 module.exports = routes;
--- api/users/users.js
+++ api/users/users.js
@@ -1,7 +1,12 @@
 const routes = require('express').Router();
 
 routes.get('/',(req,res,next)=>{
-    if(req.user) res.send({"checkLoginStatus":true})
+    // 로그인 상태확인
+    console.log(req.user)
+    if(req.user) {
+        console.log(`${req.user.username} 님이 로그인상태 요청`)
+        res.send({"checkLoginStatus":true})
+    }
     else res.send({"checkLoginStatus":false})
 })
 
